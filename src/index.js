4815e7062439cf5b271ba47c7c205d09f43fc398
SogoHlopec <sogo246@gmail.com>
2022-08-07 18:06:16 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,34 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+const arrExpr = [];
+for (let i = 0; i < expr.length; i += 10) {
+  arrExpr.push(expr.slice(i, i + 10));
+}
+const arrFinish = [];
+const end = [];
+
+arrExpr.forEach(element => {
+  let result = "";
+  for (let i = 0; i < element.length; i += 2) {
+    if (element.slice(i, i + 2) === "10") {
+      result = result + ".";
+    } else if (element.slice(i, i + 2) === "11") {
+      result = result + "-";
+    }
+  }
+  end.push(result)
+});
+
+  end.map(el => {
+  if (el === "") {
+    arrFinish.push(" ")
+  } else if (el !== " ") {
+    arrFinish.push(MORSE_TABLE[el])
+  }
+})
+
+  return arrFinish.join("");
 }
 
 module.exports = {
